b9f7d9decda3f9f164deae2519220ab32d833661
jianming <2437341214@qq.com>
2019-04-12 12:56:59 +0800

    day 19_0412 first commit

---
+++ zhexing-common-api/src/test/java/com/zhexing/common/ZhexingCommonApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhexing.common;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZhexingCommonApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
