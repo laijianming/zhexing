dc1a858952e856750ff8d209e4ff813e074a51a6
jianming <2437341214@qq.com>
2019-04-20 16:19:30 +0800

    day 19_0420 jianming commit search module

---
+++ zhexing-search/src/main/java/com/zhexing/zhexingsearch/ZhexingSearchApplication.java
@@ -0,0 +1,13 @@
+package com.zhexing.zhexingsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhexingSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhexingSearchApplication.class, args);
+    }
+
+}
---
+++ zhexing-search/src/test/java/com/zhexing/zhexingsearch/ZhexingSearchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhexing.zhexingsearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZhexingSearchApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
