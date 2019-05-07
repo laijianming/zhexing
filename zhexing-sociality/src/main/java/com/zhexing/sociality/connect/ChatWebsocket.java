80f4753c8d8a8834ccd80aac4920920626297f59
jianming <2437341214@qq.com>
2019-05-07 19:16:59 +0800

    day 19_0507 jianming commit

--- zhexing-sociality/src/main/java/com/zhexing/sociality/connect/ChatWebsocket.java
+++
@@ -1,7 +0,0 @@
-package com.zhexing.sociality.connect;
-
-/**
- *  建立与人私信的连接。
- */
-public class ChatWebsocket {
-}
--- zhexing-sociality/src/main/java/com/zhexing/sociality/connect/Websocket.java
+++
@@ -1,7 +0,0 @@
-package com.zhexing.sociality.connect;
-
-/**
- *  用户登入后建立连接。实现消息推送功能。
- */
-public class Websocket {
-}
